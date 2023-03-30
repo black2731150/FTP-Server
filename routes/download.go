81391842f52285ca586905fb8a54df43d04a5ef2
qcloud <ubuntu@localhost.localdomain>
2023-03-30 10:31:28 +0800

    优化文件下载功能，现在可以分批传输大文件，不占用太大缓存空间

--- routes/download.go
+++ routes/download.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"fpt/common"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,20 +10,6 @@ import (
 
 func DownLAndFile() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// ctx.Header("Content-Type", "application/octet-stream")
-		// ctx.Header("Content-Disposition", "attachment; filename=test") // 用来指定下载下来的文件名
-		// ctx.Header("Content-Transfer-Encoding", "text")
-		// ctx.File("./files/upload.go")
-
-		// var content string
-		// content = "hello world, 我是一个文件，" + content
-
-		// ctx.Writer.WriteHeader(http.StatusOK)
-		// ctx.Header("Content-Disposition", "attachment; filename=hello.txt")
-		// ctx.Header("Content-Type", "application/text/plain")
-		// ctx.Header("Accept-Length", fmt.Sprintf("%d", len(content)))
-		// ctx.Writer.Write([]byte(content))
-
 		filename := ctx.Param("FileName")
 
 		file, err := common.OpenFile("files/" + filename)
@@ -31,21 +18,21 @@ func DownLAndFile() gin.HandlerFunc {
 		}
 		defer file.Close()
 
-		filestat, err := file.Stat()
-		if err != nil {
-			ctx.String(http.StatusOK, "Error")
-		}
-
-		len := filestat.Size()
-
-		b := make([]byte, len)
-		n, err := file.Read(b)
-		if err != nil {
-			return
-		}
+		chuck := 1024 * 4
 
 		ctx.Header("Content-Type", "application/octet-stream")
-		ctx.Writer.Write(b[:n])
 
+		for {
+			buf := make([]byte, chuck)
+			n, err := file.Read(buf)
+			if err != nil {
+				ctx.String(http.StatusOK, "Error")
+			}
+			if err == io.EOF {
+				break
+			}
+
+			ctx.Writer.Write(buf[:n])
+		}
 	}
 }
